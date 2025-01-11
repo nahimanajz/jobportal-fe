9b197563b30f2e3c598dc5807587e6f87f08a168
nahimanajz <jazzonahimana@gmail.com>
2025-01-11 23:59:24 +0200

    fix logout button

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    output: "export",
+    basePath:"/jobportal-fe",
+    reactStrictMode: true
 };
 
 export default nextConfig;
