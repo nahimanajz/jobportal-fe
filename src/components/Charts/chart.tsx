a0dfa2e390184ed5b88b40212380ab6c4e4d42cb
nahimanajz <jazzonahimana@gmail.com>
2025-01-11 23:59:19 +0200

    deploy

--- src/components/Charts/chart.tsx
+++ src/components/Charts/chart.tsx
@@ -4,12 +4,10 @@ import { OvertimeResponse } from '@/types/IDashboardResponse';
 import { FC } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
-
-
  interface ChartProps {
     data: OvertimeResponse[]
  }
-  
+ 
 const Chart:FC<ChartProps> = ({data}) => {
     return (
         <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
--- src/components/Dashboard/Page.tsx
+++ src/components/Dashboard/Page.tsx
@@ -9,11 +9,9 @@ import { OvertimeResponse } from "@/types/IDashboardResponse";
 import Jobs from "../Tables/Jobs";
 import { getAll, getCategories } from "@/app/services/jobs";
 
-
 const Page = () => {
-  const [interval, setInterval] = useState("day")
-  const [category, setCategory] = useState<string>()
-
+  const [interval, setInterval] = useState("day");
+  const [category, setCategory] = useState<string>();
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["applicationsOvertime", interval],
@@ -21,34 +19,41 @@ const Page = () => {
   });
 
   // Jobs
-  const { data: jobs, isLoading: loading, error: jobErr } = useQuery({
+  const {
+    data: jobs,
+    isLoading: loading,
+    error: jobErr,
+  } = useQuery({
     queryKey: ["jobs", category],
     queryFn: getAll,
   });
-  // job categorues
-  const { data: categories, isLoading: catLoading, error: catErr } = useQuery({
+ 
+  
+  const {
+    data: categories,
+    isLoading: catLoading,
+    error: catErr,
+  } = useQuery({
     queryKey: ["categories"],
     queryFn: getCategories,
   });
-
   const doOnchange = (e: ChangeEvent<HTMLSelectElement>) => {
-
-    setInterval(e.target.value)
-  }
+    setInterval(e.target.value);
+  };
   const onChangeSelectOption = (e: ChangeEvent<HTMLSelectElement>) => {
-    setCategory(e.target.value)
-  }
-
-  const categoryOptions = categories?.map((item) => item.category).splice(0, 10)
+    setCategory(e.target.value);
+  };
+  const categoryOptions = categories
+    ?.map((item) => item.category)
+    .splice(0, 10);
 
   return (
-    <div className="flex md:flex-row flex-col w-full justify-between space-y-8 md-space-y-0">
-      <div className="bg-white px-8 py-3 shadow w-full">
+    <div className="md-space-y-0 flex w-full flex-col justify-between space-y-8 md:flex-row">
+      <div className="w-full bg-white px-8 py-3 shadow">
         <div className="flex justify-between bg-white">
           <span className="text-[22px] font-semibold leading-[33px] text-[#071C50] dark:text-white ">
             Job applications overtime
           </span>
-
         </div>
 
         <div className="mt-12 flex flex-col justify-start gap-4">
@@ -56,37 +61,31 @@ const Page = () => {
             <div className="w-1/2">
               <SelectGroupOne
                 options={["day", "week", "month", "year"]}
-                label={"Filter Applications by date range"} name={"interval"}
+                label={"Filter Applications by date range"}
+                name={"interval"}
                 onChange={doOnchange}
               />
             </div>
           </span>
-
         </div>
         <div className="mt-12 flex flex-col justify-between gap-8">
-
           <Chart data={data as unknown as OvertimeResponse[]} />
         </div>
       </div>
-      <div className="flex gap-8 w-full justify-end">
-        <div className="shadow bg-white px-6 py-5">
-       
-          {categories &&
+      <div className="flex w-full justify-end gap-8">
+        <div className="bg-white px-6 py-5 shadow">
+          {categories && (
             <SelectGroupOne
               options={categoryOptions as string[]}
               label={"Job Categories"}
               name={"jobs"}
               onChange={onChangeSelectOption}
-
-            />}
+            />
+          )}
           <Jobs jobs={jobs?.jobs as Job[]} />
-
         </div>
-      
-
       </div>
     </div>
-
   );
 };
 
